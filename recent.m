ff4759ada6dbb5decd9a1d398b83290c8d14a2c0
ALPAJOSE <ALPAJOSE@users.noreply.github.com>
2017-02-18 05:06:34 +0530

    Add files via upload

--- recent.m
+++ recent.m
@@ -22,7 +22,7 @@ function varargout = recent(varargin)
 
 % Edit the above text to modify the response to help recent
 
-% Last Modified by GUIDE v2.5 16-Feb-2017 09:50:42
+% Last Modified by GUIDE v2.5 18-Feb-2017 03:20:48
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -46,31 +46,31 @@ end
 
 % --- Executes just before recent is made visible.
 function recent_OpeningFcn(hObject, eventdata, handles, varargin)
-% This function has no output args, see OutputFcn.
+% This function has no output1 args, see OutputFcn.
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 % varargin   command line arguments to recent (see VARARGIN)
 
-% Choose default command line output for recent
-handles.output = hObject;
+% Choose default command line output1 for recent
+handles.output1 = hObject;
 
 % Update handles structure
 guidata(hObject, handles);
 
 % UIWAIT makes recent wait for user response (see UIRESUME)
-% uiwait(handles.figure1);
+% uiwait(handles.output2);
 
 
 % --- Outputs from this function are returned to the command line.
 function varargout = recent_OutputFcn(hObject, eventdata, handles) 
-% varargout  cell array for returning output args (see VARARGOUT);
+% varargout  cell array for returning output1 args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-% Get default command line output from handles structure
-varargout{1} = handles.output;
+% Get default command line output1 from handles structure
+varargout{1} = handles.output1;
 
 
 % --- Executes on button press in pushbutton1.
@@ -78,31 +78,79 @@ function pushbutton1_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton1 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-axes(handles.axes1)
 
-I=imread('m.png')
+axes(handles.axes5);
+i=imread('ref1.png');
+o = rgb2gray(i);
+h=edge(o,'sobel');
+
+g=100
+disp(g)
+imshow(h);
+
+axes(handles.axes1);
+I=imread('m.png');
 J = rgb2gray(I);
 p=edge(J,'sobel');
-
 q=bwarea(p)
+imshow(p);
+u1=imsubtract(q,g);
+per1=((u1-g)/u1)
+M=per1*100
+%set((handles.text1),'string',per1)
 
-imshow(p)
-axes(handles.axes2)
-L=imread('k.png')
-imshow(L)
- 
-
+axes(handles.axes2);
+L=imread('k.png');
+imshow(L);
 J1 = rgb2gray(L);
-
-
 p1=edge(J1,'sobel');
+w=bwarea(p1);
+imshow(p1);
+u2=imsubtract(w,g)
+per2=((u2-g)/u2)
 
-w=bwarea(p1)
-imshow(p1)
+M1=per2*100
+disp(M1)
+%set((handles.output1),'string',per1)
 
-u=imsubtract(p,p1);
 
 
-percentage=((w-q)/q)*100
-disp(percentage)
-set((handles.text1),'string',percentage)
+%u=imsubtract(p1,p);
+
+
+
+axes(handles.axes3);
+e=imread('2.jpg');
+r = rgb2gray(e);
+k=edge(r,'sobel');
+y=bwarea(k);
+imshow(k);
+u3=imsubtract(y,g)
+
+per3=((u3-g)/u3)
+M3=per3*100
+
+axes(handles.axes6);
+e1=imread('1.jpg');
+r1 = rgb2gray(e1);
+k1=edge(r1,'sobel');
+y1=bwarea(k1);
+imshow(k1);
+u4=imsubtract(y1,g)
+per4=((u4-g)/u4)
+M4=per4*100
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
